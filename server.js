9333a5c022dcf14ca96249696a8dbb76d400e81c
Matt Gershowitz <mattgershowitz1@gmail.com>
2017-02-16 11:27:27 -0500

    server up

---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict'
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const { itsSunnyNow } = require('./models/sunny');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'views', 'index.html'))
+});
+
+app.get('/api', itsSunnyNow, (req, res) =>{
+  res.json(res.episode);
+});
+
+app.listen(port, ()=>{ console.log(`server is doing its thang at ${port}`) });
+
+
+
+
